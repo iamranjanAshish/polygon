b46b62a0d3ee98897c65dc36b64d0fc6feb15512
Ayush Ranjan <35161810+iushranjan@users.noreply.github.com>
2024-06-12 18:26:11 +0530

    Update polygon.py

--- polygon.py
+++ polygon.py
@@ -1,14 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-circle_theta = np.linspace(0,2*3.14,747)
+plt.style.use('fivethirtyeight')
 
-# Main Function
+alpha = np.linspace(0,2*np.pi,747) # Circular Angle
 
 def polygon(sides):
 
     # Parameters & Variables
-
     initial_x = -1.0
     x_values = []
     y_values = []
@@ -18,53 +17,47 @@ def polygon(sides):
     delta_theta = internal_angle_rd/(sides - 2)
     new_theta = internal_angle_rd/2
 
-    # Tools
-
+    # Intersection Calculator
     def intersection(x):
-        while x != 1 or x < 1:
+        while x < 1:
             if (x**2 + (np.tan(new_theta)*(x + 1))**2 - 1) < 0:
-                x = x + 0.00001
+                x = x + 1e-5
                 if x == 1 or x > 1:
                     x_values.append(1)
                     y_values.append(0)
                     break
-            elif -0.000025 <= (x**2 + (np.tan(new_theta)*(x + 1))**2 - 1) <= 0.000025:
+            elif -25e-6 <= (x**2 + (np.tan(new_theta)*(x + 1))**2 - 1) <= 25e-6:
                 x_values.append(x)
                 y_values.append(np.tan(new_theta)*(x + 1))
-                x = x + 0.00001
+                x = x + 1e-5
             else:
                 break
         return None
 
-    # Actual Function
-
-    while theta + new_theta > 0.001:
+    # Rotation Loop
+    while theta + new_theta > 1e-3:
         intersection(initial_x)
         new_theta = new_theta - delta_theta
-        if theta + new_theta < 0.001:
+        if theta + new_theta < 1e-3:
             intersection(initial_x)
+            
     # Return
-
     return x_values,y_values,sides
 
-# ___________________________________________________________________________________________________
 #----------------------------------------------------------------------------------------------------
 
-x_cor, y_cor, sides = polygon(7) # you may add the number of sides over here, polygon(sides)
+x_cor, y_cor, sides = polygon(7) # polygon(Sides)
 
-#____________________________________________________________________________________________________ 
 #----------------------------------------------------------------------------------------------------
 
-# Cleaning & Arranging
-
+# Processing
 cleaned_x_cor = []
 cleaned_y_cor = []
 for i in range(len(x_cor)):
     if x_cor[i] != -1:
         cleaned_x_cor.append(x_cor[i])
-    else:
-        if i == 0:
-            cleaned_x_cor.append(x_cor[i])
+    elif i == 0:
+        cleaned_x_cor.append(x_cor[i])
 for i in range(len(y_cor)):
     if y_cor[i] != 0:
         cleaned_y_cor.append(y_cor[i])
@@ -80,7 +73,6 @@ cleaned_x_cor.append(final_segment_x)
 cleaned_y_cor.append(final_segment_y)
 
 # Polygon
-
 plt.figure(figsize=[18,8])
 
 plt.subplot(1,2,1)
@@ -89,7 +81,7 @@ plt.axhline(0, color = '#00b894', linewidth = 1)
 plt.axvline(0, color = '#00b894', linewidth = 1)
 plt.xlim(-1.1,1.1)
 plt.ylim([-1.1,1.1])
-plt.plot(np.cos(circle_theta), np.sin(circle_theta), color = '#6c5ce7', linewidth = 1)
+plt.plot(np.cos(alpha), np.sin(alpha), color = '#6c5ce7', linewidth = 1)
 plt.plot(cleaned_x_cor,cleaned_y_cor, linewidth = 0.8, color='#636e72')
 plt.grid(False)
 
@@ -104,64 +96,8 @@ if sides > 16:
     else:
         plt.xlim(-1.007,-(0.9937)**sides)
         plt.ylim([-0.03,(0.98)**sides])
-    plt.plot(np.cos(circle_theta), np.sin(circle_theta), color = '#6c5ce7', linewidth = 1.7)
+    plt.plot(np.cos(alpha), np.sin(alpha), color = '#6c5ce7', linewidth = 1.7)
     plt.plot(cleaned_x_cor,cleaned_y_cor, linewidth = 1.7, color='#636e72')
     plt.grid(False)
 
-plt.style.use('fivethirtyeight')
 plt.show()
-
-# Optional Section
-
-# You may use the code below, if you want the triangular division of sides
-# -- c means the statement was already a comment, not part of the actual code
-
-# Repetition of Elements -- c
-
-# repetition_no = 2
-# cleaned_x_center = list(np.repeat(cleaned_x_cor,repetition_no))
-# cleaned_y_center = list(np.repeat(cleaned_y_cor,repetition_no))
-
-# Coordinates of Triangles -- c
-
-# for i in range(3*len(cleaned_x_cor)):
-#     if i != len(cleaned_x_center)-1:
-#         if cleaned_x_center[i] == cleaned_x_center[i+1]:
-#             if i >= 2:
-#                 if cleaned_x_center[i] != cleaned_x_center[i-2]:
-#                     cleaned_x_center.insert(i+1,0)
-#                     cleaned_y_center.insert(i+1,0)     
-#     else:
-#         break
-
-# Polygon with Triangular Division -- c
-
-# plt.figure(figsize=[18,8])
-
-# plt.subplot(1,2,1)
-
-# plt.xlim(-1.1,1.1)
-# plt.ylim([-1.1,1.1])
-# plt.plot(np.cos(circle_theta), np.sin(circle_theta), color = '#6c5ce7', linewidth = 1)
-# plt.plot(cleaned_x_center,cleaned_y_center, linewidth = 0.8, color='#636e72')
-# plt.axhline(0, color = '#a29bfe', linewidth = 3)
-# plt.axvline(0, color = '#a29bfe', linewidth = 3)
-# plt.grid(False)
-
-# if sides > 16:
-#     plt.subplot(1,2,2)
-
-#     plt.axhline(0, color = '#81ecec', linewidth = 0.5)
-#     plt.axvline(0, color = '#81ecec', linewidth = 0.5)
-#     if 17 <= sides <= 33:
-#         plt.xlim(-1.013,-(0.987)**sides)
-#         plt.ylim([-0.03,(0.95)**sides])
-#     else:
-#         plt.xlim(-1.007,-(0.9937)**sides)
-#         plt.ylim([-0.03,(0.98)**sides])
-#     plt.plot(np.cos(circle_theta), np.sin(circle_theta), color = '#6c5ce7', linewidth = 1.7)
-#     plt.plot(cleaned_x_center,cleaned_y_center, linewidth = 1.7, color='#636e72')
-#     plt.grid(False)
-
-# plt.style.use('fivethirtyeight')
-# plt.show()
